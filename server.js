d947c8aab0ec09165f4864ac1e6dfa61d903f6a4
Manna Sandhu <manna@Mannas-MacBook-Pro.local>
2023-06-26 14:48:52 +0530

    first commit

--- server.js
+++ server.js
@@ -3,5 +3,14 @@ const app = express();
 const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+const {v4:uuidv4} = require("uuid")
 
-server.listen(3030);
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get("/:room", (req, res) => {
+    res.render("index", {roomId:req.params.room})
+})
+
+server.listen(3030); 
